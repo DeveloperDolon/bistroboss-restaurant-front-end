386ad6372d07c4996147534dec61f6941a27d748
Dolon Roy <dolonr718@gmail.com>
2023-11-15 20:06:57 +0600

    total price display on cart page commit

--- src/Pages/Cart/Cart.jsx
+++ src/Pages/Cart/Cart.jsx
@@ -8,6 +8,10 @@ import CartItem from "./CartItem";
 const Cart = () => {
 
     const {data, isLoading} = useAccessCart();
+
+    const totalPrice = data?.reduce((accumulator, currentItem) => {
+        return accumulator + currentItem.product_price;
+    }, 0);
     
     return (
         <div>
@@ -18,12 +22,22 @@ const Cart = () => {
             ></BannerCover>
 
             {
-                isLoading ? <div className="md:my-52 my-32 flex justify-center items-center"><PacmanLoader color="#36d7b7" /></div> : 
+                isLoading ? <div className="md:my-52 my-32 flex justify-center items-center"><PacmanLoader color="#36d7b7" /></div> : <>
                 <div className="max-w-7xl md:my-28 my-24 mx-auto lg:px-0 md:px-5 px-3 grid md:grid-cols-2 grid-cols-1 gap-8">
                 {
                     data?.map( item => <CartItem key={item._id} data={item}></CartItem>)
                 }
-            </div>
+                </div>
+
+                <div>
+                    <div className="divider divider-start w-full"></div>
+
+                    <div className="max-w-7xl mx-auto lg:px-0 md:px-5 px-3 pb-16">
+                        <h1 className="md:text-5xl text-3xl font-semibold">Total Price : $ {totalPrice}</h1>
+                        <button className="btn bg-amber-600 text-white mt-5 hover:text-black">Order Now</button>
+                    </div>
+                </div>
+                </>
             }
         </div>
     );
